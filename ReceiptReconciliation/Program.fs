267505ed48b1d2d77f7e69bd7bde03771198651e
Andrew M Olney <aolney@memphis.edu>
2017-09-23 08:56:05 -0500

    initial commit

---
+++ ReceiptReconciliation/Program.fs
@@ -0,0 +1,19 @@
+﻿type Receipt =
+    {
+        FileName : string
+        FileDate : System.DateTime
+        Charges : float array option
+        Dates : float array option
+    }
+
+let moneyRegex = new System.Text.RegularExpressions.Regex("[0-9]+[., ][0-9][0-9]")
+let dateRegex = new System.Text.RegularExpressions.Regex("[0-9][0-9]/[0-9][0-9]/[0-9][0-9]")
+
+let ReadReceipt( filePath ) =
+    //82817 758 PM Office Lens.txt
+    
+    let fileDate = new System.DateTime()
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
